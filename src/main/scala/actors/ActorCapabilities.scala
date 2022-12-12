0f74c91c4b48b96558fd784e571e2f6f4246687d
Jamie Sadler <jamietsadler@hotmail.co.uk>
2022-12-12 11:23:41 +0000

    Actor capabilities

--- src/main/scala/actors/ActorCapabilities.scala
+++ src/main/scala/actors/ActorCapabilities.scala
@@ -1,13 +1,50 @@
 package actors
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 object ActorCapabilities extends App {
 
   class SimpleActor extends Actor {
     override def receive: Receive = {
-      case message: String => println(s"[simple actor] I have received $message")
+      case "Hi" => sender() ! "Hello there"
+      case message: String => println(s"[self] I have received $message")
+      case number: Int => println(s"[simple actor] I have received a $number")
+      case SpecialMessage(contents) => println(s"[simple actor] I have received something special: $contents")
+      case SendMessageToYourself(content) =>
+        self ! content
+      case SayHiTo(ref) => ref ! "Hi"
+      case WirelessPhoneMessage(content, ref) => ref forward(content + "s")
     }
   }
+
+  val system = ActorSystem("actorCapabilitiesDemo")
+  val simpleActor = system.actorOf(Props[SimpleActor], "simpleActor")
+
+  simpleActor ! "hello, actor"
+
+  simpleActor ! 42
+
+  case class SpecialMessage(contents: String)
+  simpleActor ! SpecialMessage("Some special content")
+
+  // actors have information about their context and about themselves
+
+  case class SendMessageToYourself(content: String)
+  simpleActor ! SendMessageToYourself("I am an actor...")
+
+  // actors can respond to messages
+  val alice = system.actorOf(Props[SimpleActor], "alice")
+  val bob = system.actorOf(Props[SimpleActor], "bob")
+
+  case class SayHiTo(ref: ActorRef)
+  alice ! SayHiTo(bob)
+
+  alice ! "Hi"
+
+  // forwarding messages - sending a message with the original sender
+  case class WirelessPhoneMessage(content: String, ref: ActorRef)
+  alice ! WirelessPhoneMessage("Hi", bob)
+
+
 
 }
